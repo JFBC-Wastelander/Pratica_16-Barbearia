10b508649cb4aba77972839241300c3a4dea6d2e
Jean <JFBimCopinski@gmail.com>
2024-08-29 10:30:41 -0300

    feat: working code + prettier

--- src/controllers/cliente.js
+++ src/controllers/cliente.js
@@ -1,75 +1,77 @@
-const clientes = []
-let proxId = 1
+const clientes = [];
+let proxId = 1;
 
 const model = (body, id = proxId++) => {
-    let telefone = body.telefone
+  let telefone = body.telefone
     .replaceAll("-", "")
     .replaceAll("(", "")
     .replaceAll(")", "")
-    .replaceAll(" ", "")
+    .replaceAll(" ", "");
 
-    let soNumeros = true
-    telefone.split("").forEach(el => {
-        if(isNaN(Number(el))) {
-            soNumeros = false
-        }
-    })
-
-    if (body.nome != undefined &&
-        body.email != undefined &&
-        telefone != undefined &&
-        body.nome != "" &&
-        body.email != "" &&
-        telefone != "" &&
-        body.email.includes("@") &&
-        telefone.length >= 11 &&
-        telefone.length <= 12 &&
-        soNumeros) {
-        return {
-            id,
-            telefone: telefone,
-            nome: body.nome,
-            email: body.email,
-            senha: body.senha
-        }
+  let soNumeros = true;
+  telefone.split("").forEach((el) => {
+    if (isNaN(Number(el))) {
+      soNumeros = false;
     }
-}
+  });
+  if (
+    body.nome != undefined &&
+    body.email != undefined &&
+    telefone != undefined &&
+    body.nome != "" &&
+    body.email != "" &&
+    telefone != "" &&
+    body.email.includes("@") &&
+    telefone.length >= 11 &&
+    telefone.length <= 12 &&
+    soNumeros
+  ) {
+    return {
+      id,
+      telefone: telefone,
+      nome: body.nome,
+      email: body.email,
+      senha: body.senha,
+    };
+  }
+};
 
 const store = (body) => {
-    let novo = model(body)
-    if (novo) {
-        clientes.push(novo)
-        return 201
-    }
-    return 400
-}
+  let novo = model(body);
+  if (novo) {
+    clientes.push(novo);
+    return 201;
+  }
+  return 400;
+};
 
-const index = () => clientes
+const index = () => clientes;
 
-const show = (id) => clientes.find((el) => el.id == id)
+const show = (id) => clientes.find((el) => el.id == id);
 
-const update = (id, body) => {
-    let indice = clientes.find((el) => el.id == id)
-    let novo = model(body, parseInt(id))
-    if (novo && indice != -1) {
-        clientes[indice]= novo
-        return 200
-    }
-    return 400
-}
+const update = (body, id) => {
+  let indice = clientes.findIndex((el) => el.id == id);
+  let novo = model(body, parseInt(id));
+  console.log(novo);
+  if (novo && indice != -1) {
+    clientes[indice] = novo;
+    return 200;
+  }
+  return 400;
+};
 
 const destroy = (id) => {
-    let indice = clientes.find((el) => el.id == id)
+  let indice = clientes.findIndex((el) => el.id == id);
 
-    if (indice != -1){
-        clientes.splice(indice, 1)
-    }
-}
+  if (indice != -1) {
+    clientes.splice(indice, 1);
+  }
+};
 
 module.exports = {
-    store,
-    index,
-    show,
-    update,
-    destroy
-}
+  store,
+  index,
+  show,
+  update,
+  destroy,
+};
--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -1,54 +1,58 @@
-const usuarios = []
-let proxId = 1
+const usuarios = [];
+let proxId = 1;
 
-const model = (usuario, id = proxId++) => {
-    if (ususario.email != "" &&
-    usuario.email != undefined &&
-    usuario.senha != "" &&
-    usuario.senha != undefined) {
-        return {
-            id,
-            email,
-            senha
-        }
-    }
-}
+const model = (body, id = proxId++) => {
+  if (
+    body.email != "" &&
+    body.email != undefined &&
+    body.email.includes("@") &&
+    body.senha != "" &&
+    body.senha != undefined &&
+    body.senha.length >= 8
+  ) {
+    return {
+      id,
+      email: body.email,
+      senha: body.senha,
+    };
+  }
+};
 
 const store = (body) => {
-    let novo = model(body)
-    if (novo) {
-        usuarios.push(body)
-        return 201
-    }
-    return 400
-}
+  let novo = model(body);
+  if (novo) {
+    usuarios.push(novo);
+    return 201;
+  }
+  return 400;
+};
 
-const index = () => usuarios
+const index = () => usuarios;
 
-const show = (id) => usuarios.find((el) => el.id == id)
+const show = (id) => usuarios.find((el) => el.id == id);
 
-const update = (id, body) => {
-    let index = usuarios.find((el) => el.id == id)
-    let novo = model(body, parseInt(id))
-    if (novo && indes != -1) {
-        usuarios[index]= novo
-        return 200
-    }
-    return 400
-}
+const update = (body, id) => {
+  let indice = usuarios.findIndex((el) => el.id == id);
+  let novo = model(body, parseInt(id));
+  if (novo && indice != -1) {
+    usuarios[indice] = novo;
+    return 200;
+  }
+  return 400;
+};
 
 const destroy = (id) => {
-    let index = usuarios.find((el) => el.id == id)
+  let indice = usuarios.findIndex((el) => el.id == id);
 
-    if (index != -1){
-        usuarios.splice(index, 1)
-    }
-}
+  if (indice != -1) {
+    usuarios.splice(indice, 1);
+  }
+};
 
 module.exports = {
-    store,
-    index,
-    show,
-    update,
-    destroy
-}
+  store,
+  index,
+  show,
+  update,
+  destroy,
+};
